89baf27f1a94ed2840b3207ce7205b16b1a03cdb
Natdenice <Clad1987@hotmail.com>
2018-10-30 21:51:10 +0100

    Last commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
   def new
-  @user = User.new('username' => params[:username],
-                  'email' => params[:email],
-                  'bio' => params[:bio])
+    @user = User.new('username' => params[:username],
+      'email' => params[:email],
+      'bio' => params[:bio])
   end
 
   def create
-  @user = User.create!(username: params[:username], email:params[:email], bio: params[:bio])	
+    if params['username']
+      @user = User.create!(
+        username: params[:username],
+        email: params[:email],
+        bio: params[:bio]
+        )
+
+    elsif params['user']['username']
+      @user = User.create(
+        username: params["user"]["username"],
+        email: params["user"]["email"],
+        bio: params["user"]["bio"]
+        )
+    end
   end
 end
